bcac0dfb7ac9b481cf940181ca470dccf0541f82
skyZY <303986887@qq.com>
2017-05-09 00:46:54 +0800

    提交accessibilityservice初始类
    创建模拟点击service

---
+++ app/src/main/java/com/grapmoney/plugin/grap/RabMoneyService.java
@@ -0,0 +1,21 @@
+package com.grapmoney.plugin.grap;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by Administrator on 2017/5/9.
+ */
+
+public class RabMoneyService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
